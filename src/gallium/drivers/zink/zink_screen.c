04d0dc08e8c981b96177944a7913d9ab806d0536
ocnedkf <143856368+ocnedkf@users.noreply.github.com>
2025-07-09 23:58:49 +0800

    Fix the xml_config error​
    Remove unused commented code for driParseConfigFiles and driQueryOptionb

--- src/gallium/drivers/zink/zink_screen.c
+++ src/gallium/drivers/zink/zink_screen.c
@@ -2139,11 +2139,11 @@ zink_internal_create_screen(const struct pipe_screen_config *config)
    screen->instance_info.loader_version = zink_get_loader_version(screen);
 #if WITH_XMLCONFIG
    if (config) {
-      driParseConfigFiles(config->options, config->options_info, 0, "zink",
-                          NULL, NULL, NULL, 0, NULL, 0);
-      screen->driconf.dual_color_blend_by_location = driQueryOptionb(config->options, "dual_color_blend_by_location");
+    //  driParseConfigFiles(config->options, config->options_info, 0, "zink",
+     //                     NULL, NULL, NULL, 0, NULL, 0);
+ //     screen->driconf.dual_color_blend_by_location = driQueryOptionb(config->options, "dual_color_blend_by_location");
       //screen->driconf.inline_uniforms = driQueryOptionb(config->options, "radeonsi_inline_uniforms");
-      screen->instance_info.disable_xcb_surface = driQueryOptionb(config->options, "disable_xcb_surface");
+  //    screen->instance_info.disable_xcb_surface = driQueryOptionb(config->options, "disable_xcb_surface");
    }
 #endif
 
